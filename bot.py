6cb2bfb9b9c4cb7b45d07563cea9d77584a1e5ba
KALI-OFFICIAL1 <kaliofficial448@gmail.com>
2025-04-06 14:09:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ from pyrogram.types import Message
 import random
 
 # --- Config ---
-API_ID = 12345678  # Replace with your API ID
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
+API_ID = "21546320"  # Replace with your API ID
+API_HASH = "c16805d6f2393d35e7c49527daa317c7"
+BOT_TOKEN = "7789344228:AAGLvdcqj70nezj817gJ9RstrAtrD9IMvpo"
 
 # --- Emoji Stats ---
 ANIMAL_STATS = {
